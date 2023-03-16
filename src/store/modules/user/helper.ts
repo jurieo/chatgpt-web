efeef5f7d8417372f415ec589c23e09888e1b2bf
陈俊霖 <junlin.chen2@geely.com>
2023-03-16 14:08:58 +0800

    fix: ops

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -14,12 +14,12 @@ export interface UserState {
 
 export function defaultSetting(): UserState {
   return {
-    userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
-    },
-  }
+		userInfo: {
+			avatar: "https://oss.xuepg.com/Xpg/avatar.jpeg",
+			name: "Jurieo-bot",
+			description: "",
+		},
+	};
 }
 
 export function getLocalState(): UserState {
